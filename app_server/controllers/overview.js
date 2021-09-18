4542a9309d584ffe2ecf5ca31570e64942cfbd18
RaiDiaz <raidiaz1941@gmail.com>
2021-09-18 11:19:32 -0500

    Ventanas y controladores actualizadas

--- app_server/controllers/overview.js
+++ app_server/controllers/overview.js
@@ -1,7 +1,25 @@
 /* GET ovrview. */
 const overview = (req, res) => {
-    res.render('overview',{title: 'overview '});
+    res.render('overview',{
+        title: 'overview ',
+        pageUser: {
+            user: 'User',
+            plan: 'Starter'
+        },
+        ticket_menu: [{
+            Date: '2021/05/18',
+            Topic: 'Error',
+            Status: 'Pending'
+        },{
+            Date: '2021/04/28',
+            Topic: 'Error',
+            Status: 'Solved'
+        }]
+    });
 };
+
+
+
 
 module.exports = {
     overview
--- app_server/controllers/ticket.js
+++ app_server/controllers/ticket.js
@@ -1,6 +1,23 @@
 /* GET ovrview. */
 const ticket = (req, res) => {
-    res.render('ticket',{title: 'ticket '});
+    res.render('ticket',{
+        title: 'ticket ',
+        ticketWin: [{
+            Date: '2021/05/18',
+            Name: 'Juan',
+            Topic: 'Error',
+            Descripcion: 'Modulo impresion',
+            Responsable: 'unassigned',
+            Status: 'Pending'
+        },{
+            Date: '2021/04/28',
+            Name: 'Roberto',
+            Topic: 'Error',
+            Descripcion: 'Facturación',
+            Responsable: 'Ing. Diaz',
+            Status: 'Solved'
+        }]
+    });
 };
 
 module.exports = {
